a98e5261ae501d4b8a8cb5f751917921f5878631
Mario Samudio <13368517+mrsamudio@users.noreply.github.com>
2020-12-04 23:21:30 -0500

    Logs
    logs en AOP

--- src/main/java/co/edu/ucundinamarca/uperc/InicioSession.java
+++ src/main/java/co/edu/ucundinamarca/uperc/InicioSession.java
@@ -3,8 +3,10 @@
  */
 package co.edu.ucundinamarca.uperc;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * Inicio de sesion
@@ -13,12 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 0.0.1
  */
 @Controller
+@Component
 public class InicioSession {
+	
 	
 	@RequestMapping
 	public String inicioDeSession() {
-		
 		return "InicioDeSesion";		
 	}
-
 }
