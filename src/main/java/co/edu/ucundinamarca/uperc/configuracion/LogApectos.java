c3022b2aaa3ec92031cd88ccd36886d8c0a84c39
Mario Samudio <13368517+mrsamudio@users.noreply.github.com>
2020-12-07 16:28:23 -0500

    Logs
    AJustes en AOP

--- src/main/java/co/edu/ucundinamarca/uperc/configuracion/LogApectos.java
+++ src/main/java/co/edu/ucundinamarca/uperc/configuracion/LogApectos.java
@@ -1,4 +1,4 @@
-package co.edu.ucundinamarca.uperc;
+package co.edu.ucundinamarca.uperc.configuracion;
 
 
 import org.apache.logging.log4j.LogManager;
@@ -6,10 +6,12 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,31 +19,74 @@ import org.springframework.stereotype.Component;
  * @author mrsamudio
  *
  */
-@Aspect
 @Component
+@Aspect
 public class LogApectos {
 	
 	private static Logger logger = LogManager.getLogger(LogApectos.class);
 	
-	//TODO completar aspectos
-	@Before("execution(* co.edu.ucundinamarca.uperc.*(..))")
+	/**
+	 * Punto de corte para todos los métodos públicos
+	 */
+	@Pointcut("execution( public * co.edu.ucundinamarca.uperc.*.*(..))")
+    private void metodosPublicos() {} 	
+
+	/**
+	 * 
+	 */
+    @Pointcut("within (co.edu.ucundinamarca.uperc..*)")
+    private void enUperc() {}    
+
+    /**
+     * 
+     */
+    @Pointcut("metodosPublicos() && enUperc()")
+    private void metodosPublicosEnUperc() {}
+	
+    /**
+     * 
+     * @param joinPoint
+     */
+	@Before("within (@org.springframework.stereotype.Controller *)")
 	public void logAntes(JoinPoint joinPoint) {
-		logger.info("" + joinPoint.getSignature().getName());
+		logger.info("Antes de ejecutar " + joinPoint.getSignature().getName());
 	}
 
-	//TODO completar aspectos
-	@After("execution(* co.edu.ucundinamarca.uperc.*(..))")
+
+	/**
+	 * 
+	 * @param joinPoint
+	 */
+	@After("within (@org.springframework.stereotype.Controller *)")
 	public void logDespues(JoinPoint joinPoint) {
-		logger.info("" + joinPoint.getSignature().getName());
+		logger.info("Despues de ejecutar " + joinPoint.getSignature().getName());
 	}
 	
-	@AfterThrowing
-	public void logAAfterThrowging(JoinPoint joinPoint, Throwable excepcion) {
+//	@AfterThrowing("execution(* *.*(..))")
+//	public void logAAfterThrowging(JoinPoint joinPoint, Throwable e) {
+//		logger.info("al ejecutar " + joinPoint.getSignature().getName());
+//		logger.info("Se ha lanzado la excepción: " + e.getMessage());
+//	}
+	
+//	@Around("execution(* co.edu.ucundinamarca.uperc.*(..))")
+//	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+//		logger.info("Antes y despues @Around");
+//		logger.info("Antes de ejecutar " + joinPoint.getSignature().getName());
+//		joinPoint.proceed();
+//		logger.info("Despues de ejecutar " + joinPoint.getSignature().getName());
+//		
+//	}
+	
+	/**
+	 * Registra el
+	 * @param joinPoint
+	 * @param resultado
+	 */
+	@AfterReturning(pointcut = "metodosPublicos()", returning = "resultado")
+	public void logDespuesRes(JoinPoint joinPoint, Object resultado) {
 		
-	}
-	
-	@Around("execution(* co.edu.ucundinamarca.uperc.*(..))")
-	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		logger.info("En el método " + joinPoint.getSignature().getName());
+		logger.info("El valor retornado es: " + resultado.toString());
 		
 	}
 	
