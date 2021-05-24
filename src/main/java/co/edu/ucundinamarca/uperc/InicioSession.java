a8324d944eef6b3c5460d9dd6ee3d9e460dd018f
Mario Samudio <13368517+mrsamudio@users.noreply.github.com>
2021-05-24 12:52:16 -0500

    Fix login y chartjs scurity

--- src/main/java/co/edu/ucundinamarca/uperc/InicioSession.java
+++ src/main/java/co/edu/ucundinamarca/uperc/InicioSession.java
@@ -4,6 +4,7 @@
 package co.edu.ucundinamarca.uperc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,12 +24,17 @@ import co.edu.ucundinamarca.uperc.persistencia.entidades.Usuario;
  */
 @Controller
 @Component
+//@EnableAspectJAutoProxy
 public class InicioSession {
 	
 	@Autowired
 	private UsuarioDAO usuariorepo;
 	
-	@GetMapping(path = "/login")
+	/**
+	 * Redirección de raiz /uperc/ a login, anteriormente raiz de pagina jsp
+	 * @return
+	 */
+	@GetMapping(path = "/")
 	public String inicioDeSession() {
 //		return "InicioDeSesion";		
 		return "redirect:/login";		
@@ -49,14 +55,15 @@ public class InicioSession {
 	
 	/**
 	 * Verifica si el usuario y la contraseña ingresados se encuentran en bd
+	 * 
 	 * @param model
 	 * @param usuario
 	 * @param bindingResult
 	 * @return página de respuesta con los atributos relacionados 
 	 */
-//	@PostMapping(value="/doLogin", params={"goLogin"})
-	@PostMapping(path ="/login", params={"goLogin"})
-    public String doLogin(Model model,@ModelAttribute Usuario usuario, BindingResult bindingResult) {
+	@PostMapping(value="/doLogin", params={"goLogin"})
+//	@PostMapping(path ="/login", params={"goLogin"})
+    public String doLogin(Model model,@ModelAttribute Usuario usuario, BindingResult bindingResult) throws Exception{
 		
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("errortitulo", "Error");
@@ -65,17 +72,53 @@ public class InicioSession {
 			return "login.html";
 		}
 		
-		if (usuariorepo.selectByCorreo(usuario.getCorreo()).getContrasena().equals(usuario.getContrasena())) {
-//			TODO: agregar atributos al modelo para conservar la sessión activa
-			return "redirect:/tableroPrincipal";
-		}else {
-			model.addAttribute("errortitulo", "Ingreso fallido");
-			model.addAttribute("errormensaje", "La contraseña no es correcta, por favor intenta de nuevo");
-			model.addAttribute(Usuario.class.getName().toLowerCase(), new Usuario());
-			model.addAttribute("mostrar", "$(window).load(function(){$('#mensajeModal').modal('show');});");
-			return "login.html";
-		}
+		Usuario u = null;
+		
+		try {
+			u = usuariorepo.selectByCorreo(usuario.getCorreo());
+			
+//			testeo();
+			
+			if (u != null) {
+				if (u.getContrasena().equals(usuario.getContrasena())) {
+					//TODO: agregar atributos al modelo para conservar la sessión activa
+					return "redirect:/tableroPrincipal";
+				}else {
+					return errorDeIngreso(model, "Ingreso fallido", "La contraseña no es correcta, por favor intenta de nuevo");
+				}
+			}else {
+				return errorDeIngreso(model, "Usuario no encontrado", "El usuario no se encuentra registrado!");
+			}
+				
+		} catch (Exception e) {
+			e.getMessage();
+			return errorDeIngreso(model, "Error desconocido", "Por favor contacte al administrador!");
+		}		
+		
     }
+
+
+
+	/**
+	 * Desencadena un mensaje modal indicando el error relacionado
+	 * 
+	 * @param model
+	 * @param titulo
+	 * @param mensaje
+	 * @return
+	 */
+	private String errorDeIngreso(Model model, String titulo, String mensaje) {
+		model.addAttribute("errortitulo", titulo);
+		model.addAttribute("errormensaje", mensaje);
+		model.addAttribute(Usuario.class.getName().toLowerCase(), new Usuario());
+		model.addAttribute("mostrar", "$(window).load(function(){$('#mensajeModal').modal('show');});");
+		return "login.html";
+	}
+	
+	
+	public void testeo() throws Exception {
+		throw new Exception("Simulate an error");
+	}
 	
 	
 }
